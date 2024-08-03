53e3513213ffa8233b11c69bb43da562f70f3bcd
JEFTEDARIEL123 <jeftedariel3@gmail.com>
2024-08-02 21:37:36 -0600

    creacion de archivo lista

--- src/main/java/com/jefte/estacionmeteorologica/Main.java
+++ src/main/java/com/jefte/estacionmeteorologica/Main.java
@@ -3,7 +3,7 @@
  */
 
 package com.jefte.estacionmeteorologica;
-
+import com.jefte.estacionmeteorologica.ManejoArchivos.JsonHandler;
 /**
  *
  * @author jefte
@@ -11,6 +11,6 @@ package com.jefte.estacionmeteorologica;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
     }
 }
--- src/main/java/com/jefte/estacionmeteorologica/ManejoArchivos/JsonHandler.java
+++ src/main/java/com/jefte/estacionmeteorologica/ManejoArchivos/JsonHandler.java
@@ -7,6 +7,8 @@ package com.jefte.estacionmeteorologica.ManejoArchivos;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 /**
@@ -19,16 +21,22 @@ public class JsonHandler {
 
     //Crea archivo para definir esta vara no sé
     //Se debe pasar por parametro el nombre del archivo que se quiere crear.
-    public JsonHandler(String archivo) {
+    public JsonHandler(String nombre){
+        File archivo = new File(nombre);
         this.crearJson(archivo);
         ObjMap = new ObjectMapper();
     }
     
     //Simplemente crea el archivo
-    private void crearJson(String archivo) {
-        new File(archivo);
+    private void crearJson(File archivo) {
+        try {
+            archivo.createNewFile();
+        } catch (IOException ex) {
+            JOptionPane.showMessageDialog(null, "No se pudo crear el archivo json:"+ archivo.getName() + "\n" + ex);
+        } 
     }
     
     
-
+    
 }
+
